8db7560635c9df00c98db11e761057f2f073dfd3
David Greer <greerdavidc@gmail.com>
2015-05-15 04:40:16 -0500

    finished up AES and AES correctness demo

--- cryptoimpl/src/aes.rs
+++ cryptoimpl/src/aes.rs
@@ -1,9 +1,5 @@
 use aesconst;
 
-pub fn test() {
-    println!("test worked, you linked the mod correctly");
-}
-
 fn sub_bytes(data: &mut [u8; 16]) {
     for i in (0..data.len()) {
         data[i] = aesconst::SBOX[data[i] as usize];
@@ -17,12 +13,13 @@ fn inv_sub_bytes(data: &mut [u8; 16]) {
 }
 
 fn shift_rows(data: &mut [u8; 16]) {
+    // AES uses column-major ordering
     let tmp: [u8; 16] =  
     [
-        data[0],  data[1],  data[2],  data[3],
-        data[5],  data[6],  data[7],  data[4],
-        data[10], data[11], data[8],  data[9],
-        data[15], data[12], data[13], data[14]
+        data[0],  data[5],  data[10],  data[15],
+        data[4],  data[9],  data[14],  data[3],
+        data[8], data[13], data[2],  data[7],
+        data[12], data[1], data[6], data[11]
     ];
 
     for i in 0..data.len() {
@@ -33,10 +30,10 @@ fn shift_rows(data: &mut [u8; 16]) {
 fn inv_shift_rows(data: &mut [u8; 16]) {
     let tmp: [u8; 16] =  
     [
-        data[0],  data[1],  data[2],  data[3],
-        data[7],  data[4],  data[5],  data[6],
-        data[10], data[11], data[8],  data[9],
-        data[13], data[14], data[15], data[12]
+        data[0],  data[13],  data[10],  data[7],
+        data[4],  data[1],  data[14],  data[11],
+        data[8], data[5], data[2],  data[15],
+        data[12], data[9], data[6], data[3]
     ];
 
     for i in 0..data.len() {
@@ -45,25 +42,18 @@ fn inv_shift_rows(data: &mut [u8; 16]) {
 }
 
 fn mix_cols(data: &mut [u8; 16]) {
-    let mut tmp: [u8; 16] = [0; 16];
+    let mut tmp: [usize; 16] = [0; 16];
     for i in 0..16 {
-        tmp[i] = data[i];
+        tmp[i] = data[i] as usize;
     }
 
     for i in 0..16 {
-        let row = i / 4;
-        let col: [u8; 4] = match row {
-            0 => [ tmp[i],    tmp[i+4], tmp[i+8], tmp[i+12] ],
-            1 => [ tmp[i-4],  tmp[i],   tmp[i+4], tmp[i+8]  ],
-            2 => [ tmp[i-8],  tmp[i-4], tmp[i],   tmp[i+4]  ],
-            3 => [ tmp[i-12], tmp[i-8], tmp[i-4], tmp[i]    ],
-            _ => panic!("AES: Bad row when mixing columns: {}", row),
-        };
+        let row = i % 4;
         data[i] = match row {
-            0 => aesconst::MUL2[col[0] as usize] ^ aesconst::MUL3[col[1] as usize] ^ col[2]                          ^ col[3],
-            1 => col[0]                          ^ aesconst::MUL2[col[1] as usize] ^ aesconst::MUL3[col[2] as usize] ^ col[3],
-            2 => col[0]                          ^ col[1]                          ^ aesconst::MUL2[col[2] as usize] ^ aesconst::MUL3[col[3] as usize],
-            3 => aesconst::MUL3[col[0] as usize] ^ col[1]                          ^ col[2]                          ^ aesconst::MUL2[col[3] as usize],
+            0 => aesconst::MUL2[tmp[i]] ^ aesconst::MUL3[tmp[i+1]] ^ tmp[i+2] as u8           ^ tmp[i+3] as u8,
+            1 => tmp[i-1] as u8         ^ aesconst::MUL2[tmp[i]]   ^ aesconst::MUL3[tmp[i+1]] ^ tmp[i+2] as u8,
+            2 => tmp[i-2] as u8         ^ tmp[i-1] as u8           ^ aesconst::MUL2[tmp[i]]   ^ aesconst::MUL3[tmp[i+1]],
+            3 => aesconst::MUL3[tmp[i-3]] ^ tmp[i-2] as u8         ^ tmp[i-1] as u8           ^ aesconst::MUL2[tmp[i]],
             _ => panic!("AES: Bad row when mixing columns: {}", row),
         };
     }
@@ -71,25 +61,18 @@ fn mix_cols(data: &mut [u8; 16]) {
 }
 
 fn inv_mix_cols(data: &mut [u8; 16]) {
-    let mut tmp: [u8; 16] = [0; 16];
+    let mut tmp: [usize; 16] = [0; 16];
     for i in 0..16 {
-        tmp[i] = data[i];
+        tmp[i] = data[i] as usize;
     }
 
     for i in 0..16 {
-        let row = i / 4;
-        let col: [u8; 4] = match row {
-            0 => [ tmp[i],    tmp[i+4], tmp[i+8], tmp[i+12] ],
-            1 => [ tmp[i-4],  tmp[i],   tmp[i+4], tmp[i+8]  ],
-            2 => [ tmp[i-8],  tmp[i-4], tmp[i],   tmp[i+4]  ],
-            3 => [ tmp[i-12], tmp[i-8], tmp[i-4], tmp[i]    ],
-            _ => panic!("AES: Bad row when inverse mixing columns: {}", row),
-        };
+        let row = i % 4;
         data[i] = match row {
-            0 => aesconst::MUL14[col[0] as usize] ^ aesconst::MUL11[col[1] as usize] ^ aesconst::MUL13[col[2] as usize] ^ aesconst::MUL9[col[3] as usize],
-            1 => aesconst::MUL9[col[0] as usize]  ^ aesconst::MUL14[col[1] as usize] ^ aesconst::MUL11[col[2] as usize] ^ aesconst::MUL13[col[3] as usize],
-            2 => aesconst::MUL13[col[0] as usize] ^ aesconst::MUL9[col[1] as usize]  ^ aesconst::MUL14[col[2] as usize] ^ aesconst::MUL11[col[3] as usize],
-            3 => aesconst::MUL11[col[0] as usize] ^ aesconst::MUL13[col[1] as usize] ^ aesconst::MUL9[col[2] as usize]  ^ aesconst::MUL14[col[3] as usize],
+            0 => aesconst::MUL14[tmp[i]]   ^ aesconst::MUL11[tmp[i+1]] ^ aesconst::MUL13[tmp[i+2]] ^ aesconst::MUL9[tmp[i+3]],
+            1 => aesconst::MUL9[tmp[i-1]]  ^ aesconst::MUL14[tmp[i]]   ^ aesconst::MUL11[tmp[i+1]] ^ aesconst::MUL13[tmp[i+2]],
+            2 => aesconst::MUL13[tmp[i-2]] ^ aesconst::MUL9[tmp[i-1]]  ^ aesconst::MUL14[tmp[i]]   ^ aesconst::MUL11[tmp[i+1]],
+            3 => aesconst::MUL11[tmp[i-3]] ^ aesconst::MUL13[tmp[i-2]] ^ aesconst::MUL9[tmp[i-1]]  ^ aesconst::MUL14[tmp[i]],
             _ => panic!("AES: Bad row when inverse mixing columns: {}", row),
         };
     }
@@ -110,29 +93,30 @@ fn schedule_keys(key: & [u8; 16]) -> [[u8; 16]; 11] {
     }
 
     for i in 1..keys {
-        let mut t: [u8; 16] = [0; 16];
+        let mut t: [u8; 4] = [0; 4];
         t[0] = roundkeys[i-1][12];
         t[1] = roundkeys[i-1][13];
         t[2] = roundkeys[i-1][14];
         t[3] = roundkeys[i-1][15];
 
-        roundkeys[i][0] = aesconst::SBOX[t[1] as usize];
+        roundkeys[i][0] = aesconst::SBOX[t[1] as usize] ^ aesconst::RCON[i-1];
         roundkeys[i][1] = aesconst::SBOX[t[2] as usize];
         roundkeys[i][2] = aesconst::SBOX[t[3] as usize];
         roundkeys[i][3] = aesconst::SBOX[t[0] as usize];
 
-        roundkeys[i][0] ^= aesconst::RCON[i-1];
+        for rd in 0..4 {
+            roundkeys[i][rd] ^= roundkeys[i-1][rd];
+        }
 
         for rd in 4..16 {
             roundkeys[i][rd] = roundkeys[i][rd-4] ^ roundkeys[i-1][rd];
         }
     }
+
     roundkeys
 }
 
-pub fn encrypt_block(data: &mut[u8; 16], key: &[u8; 16]) {
-    let roundkeys: [[u8; 16]; 11] = schedule_keys(key);
-
+pub fn encrypt_block(data: &mut[u8; 16], roundkeys: &[[u8; 16]; 11]) {
     add_round_key(data, &roundkeys[0]);
 
     for i in 1..10 {
@@ -147,130 +131,34 @@ pub fn encrypt_block(data: &mut[u8; 16], key: &[u8; 16]) {
     add_round_key(data, &roundkeys[roundkeys.len()-1]);
 }
 
-pub fn decrypt_block(data: &mut[u8; 16], key: &[u8; 16]) {
-    let roundkeys: [[u8; 16]; 11] = schedule_keys(key);
-
+pub fn decrypt_block(data: &mut[u8; 16], roundkeys: &[[u8; 16]; 11]) {
     add_round_key(data, &roundkeys[roundkeys.len()-1]);
 
     for i in 1..10 {
-        inv_sub_bytes(data);
         inv_shift_rows(data);
+        inv_sub_bytes(data);
         add_round_key(data, &roundkeys[roundkeys.len()-i-1]);
         inv_mix_cols(data);
     }
-
-    inv_sub_bytes(data);
     inv_shift_rows(data);
+    inv_sub_bytes(data);
     add_round_key(data, &roundkeys[0]);
 }
 
-fn print_data(data: &mut [u8; 16]) {
-    for i in 0..data.len() {
-        print!("0x{:X}\t", data[i]);
-        if i%4 == 3 {
-            println!("");
-        }
-    }
-    println!("");
-}
-
-pub fn run_tests(data: &mut [u8; 16]) {
-    test_mix_cols(data);
-    test_sub_bytes(data);
-    test_shift_rows(data);
-    test_pkcs_pad();
-}
-
-pub fn test_mix_cols(data: &mut [u8; 16]) {
-    println!("Testing Mix Columns:");
-    
-    print_data(data);
-
-    mix_cols(data);
-
-    print_data(data);
-
-    inv_mix_cols(data);
-
-    print_data(data);
-}
-
-pub fn test_sub_bytes(data: &mut [u8; 16]) {
-    println!("Testing Sub Bytes:");
-    
-    print_data(data);
-
-    sub_bytes(data);
-
-    print_data(data);
-
-    inv_sub_bytes(data);
-
-    print_data(data);
-}
-
-pub fn test_shift_rows(data: &mut [u8; 16]) {
-    println!("Testing Shift Rows:");
-    
-    print_data(data);
-
-    shift_rows(data);
-
-    print_data(data);
-
-    inv_shift_rows(data);
-
-    print_data(data);
-}
 
 pub fn pkcs_pad(data: &mut Vec<u8>) {
     let bytes: usize = 16 - data.len()%16;
     let newlen = data.len() + bytes;
-    for i in data.len()..newlen {
+    for _ in data.len()..newlen {
         data.push(bytes as u8);
     }
 }
 
 pub fn pkcs_unpad(data: &mut Vec<u8>) {
     let removenum = data[data.len()-1];
-    for i in 0..removenum {
+    for _ in 0..removenum {
         data.pop();
     }
-}
-
-pub fn test_pkcs_pad() {
-    println!("Testing PKCS Padding:");
-
-
-    let mut data = vec![0,6,7,8,4,23,4,3,6,1];
-
-    for i in 0..data.len() {
-        print!("0x{:X}\t", data[i]);
-        if i%4 == 3 {
-            println!("");
-        }
-    }
-    println!("\n");
-
-    pkcs_pad(&mut data);
-
-    for i in 0..data.len() {
-        print!("0x{:X}\t", data[i]);
-        if i%4 == 3 {
-            println!("");
-        }
-    }
-    println!("\n");
-
-    pkcs_unpad(&mut data);
-
-    for i in 0..data.len() {
-        print!("0x{:X}\t", data[i]);
-        if i%4 == 3 {
-            println!("");
-        }
-    }
-    println!("\n");
 }
 
 pub fn cbc_encrypt(data: &[u8], k: &[u8], iv: &[u8]) -> Vec<u8> {
@@ -290,11 +178,13 @@ pub fn cbc_encrypt(data: &[u8], k: &[u8], iv: &[u8]) -> Vec<u8> {
 
     pkcs_pad(&mut vdata);
 
+    let roundkeys: [[u8; 16]; 11] = schedule_keys(&key);
+
     for i in 0..(vdata.len()/16) {
         for j in 0..16 {
             blockdata[j] = vdata[i*16 + j] ^ old[j];
         }
-        encrypt_block(&mut blockdata, &key);
+        encrypt_block(&mut blockdata, &roundkeys);
         for j in 0..16 {
             finaldata.push(blockdata[j]);
             old[j] = blockdata[j];
@@ -319,15 +209,76 @@ pub fn cbc_decrypt(data: &[u8], k: &[u8], iv: &[u8]) -> Vec<u8> {
         old[i] = iv[i];
     }
 
+    let roundkeys: [[u8; 16]; 11] = schedule_keys(&key);
+
     for i in 0..(vdata.len()/16) {
         for j in 0..16 {
             blockdata[j] = vdata[i*16 + j];
             tmpdata[j] = blockdata[j];
         }
-        decrypt_block(&mut blockdata, &key);
+        decrypt_block(&mut blockdata, &roundkeys);
         for j in 0..16 {
             finaldata.push(blockdata[j] ^ old[j]);
             old[j] = tmpdata[j];
+        }
+    }
+
+    pkcs_unpad(&mut finaldata);
+
+    finaldata
+}
+
+pub fn ecb_encrypt(data: &[u8], k: &[u8]) -> Vec<u8> {
+    let mut vdata: Vec<u8> = Vec::new();
+    let mut finaldata: Vec<u8> = Vec::new();
+    let mut blockdata: [u8; 16] = [0; 16];
+    let mut key: [u8; 16] = [0; 16];
+
+    for i in 0..data.len() {
+        vdata.push(data[i]);
+    }
+    for i in 0..16 {
+        key[i] = k[i];
+    }
+
+    pkcs_pad(&mut vdata);
+
+    let roundkeys: [[u8; 16]; 11] = schedule_keys(&key);
+
+    for i in 0..(vdata.len()/16) {
+        for j in 0..16 {
+            blockdata[j] = vdata[i*16 + j];
+        }
+        encrypt_block(&mut blockdata, &roundkeys);
+        for j in 0..16 {
+            finaldata.push(blockdata[j]);
+        }
+    }
+    finaldata
+}
+
+pub fn ecb_decrypt(data: &[u8], k: &[u8]) -> Vec<u8> {
+    let mut vdata: Vec<u8> = Vec::new();
+    let mut finaldata: Vec<u8> = Vec::new();
+    let mut blockdata: [u8; 16] = [0; 16];
+    let mut key: [u8; 16] = [0; 16];
+
+    for i in 0..data.len() {
+        vdata.push(data[i]);
+    }
+    for i in 0..16 {
+        key[i] = k[i];
+    }
+
+    let roundkeys: [[u8; 16]; 11] = schedule_keys(&key);
+
+    for i in 0..(vdata.len()/16) {
+        for j in 0..16 {
+            blockdata[j] = vdata[i*16 + j];
+        }
+        decrypt_block(&mut blockdata, &roundkeys);
+        for j in 0..16 {
+            finaldata.push(blockdata[j]);
         }
     }
 
--- test-crypto/src/main.rs
+++ test-crypto/src/main.rs
@@ -8,97 +8,29 @@ extern crate crypto;
 extern crate rand;
 extern crate cryptoimpl;
 
-use crypto::{ symmetriccipher, buffer, aes, blockmodes };
+use crypto::{ symmetriccipher, buffer, aes, blockmodes, scrypt };
 use crypto::buffer::{ ReadBuffer, WriteBuffer, BufferResult };
 
 use rand::Rng;
 
-use std::fs::File;
-use std::path::Path;
-use std::io::Write;
-
-// Encrypt a buffer with the given key and iv using
-// AES-256/CBC/Pkcs encryption.
 fn encrypt(data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
 
-    // Create an encryptor instance of the best performing
-    // type available for the platform.
     let mut encryptor = aes::cbc_encryptor(
             aes::KeySize::KeySize128,
             key,
             iv,
             blockmodes::PkcsPadding);
 
-    // Each encryption operation encrypts some data from
-    // an input buffer into an output buffer. Those buffers
-    // must be instances of RefReaderBuffer and RefWriteBuffer
-    // (respectively) which keep track of how much data has been
-    // read from or written to them.
     let mut final_result = Vec::<u8>::new();
     let mut read_buffer = buffer::RefReadBuffer::new(data);
     let mut buffer = [0; 4096];
     let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
 
-    // Each encryption operation will "make progress". "Making progress"
-    // is a bit loosely defined, but basically, at the end of each operation
-    // either BufferUnderflow or BufferOverflow will be returned (unless
-    // there was an error). If the return value is BufferUnderflow, it means
-    // that the operation ended while wanting more input data. If the return
-    // value is BufferOverflow, it means that the operation ended because it
-    // needed more space to output data. As long as the next call to the encryption
-    // operation provides the space that was requested (either more input data
-    // or more output space), the operation is guaranteed to get closer to
-    // completing the full operation - ie: "make progress".
-    //
-    // Here, we pass the data to encrypt to the enryptor along with a fixed-size
-    // output buffer. The 'true' flag indicates that the end of the data that
-    // is to be encrypted is included in the input buffer (which is true, since
-    // the input data includes all the data to encrypt). After each call, we copy
-    // any output data to our result Vec. If we get a BufferOverflow, we keep
-    // going in the loop since it means that there is more work to do. We can
-    // complete as soon as we get a BufferUnderflow since the encryptor is telling
-    // us that it stopped processing data due to not having any more data in the
-    // input buffer.
     loop {
         let result = try!(encryptor.encrypt(&mut read_buffer, &mut write_buffer, true));
 
-        // "write_buffer.take_read_buffer().take_remaining()" means:
-        // from the writable buffer, create a new readable buffer which
-        // contains all data that has been written, and then access all
-        // of that data as a slice.
         final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
 
-        match result {
-            BufferResult::BufferUnderflow => break,
-            BufferResult::BufferOverflow => { }
-        }
-    }
-
-    Ok(final_result)
-}
-
-// Decrypts a buffer with the given key and iv using
-// AES-256/CBC/Pkcs encryption.
-//
-// This function is very similar to encrypt(), so, please reference
-// comments in that function. In non-example code, if desired, it is possible to
-// share much of the implementation using closures to hide the operation
-// being performed. However, such code would make this example less clear.
-fn decrypt(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symmetriccipher::SymmetricCipherError> {
-    let mut decryptor = aes::cbc_decryptor(
-            aes::KeySize::KeySize128,
-            key,
-            iv,
-            blockmodes::PkcsPadding);
-
-    let mut final_result = Vec::<u8>::new();
-    let mut read_buffer = buffer::RefReadBuffer::new(encrypted_data);
-    let mut buffer = [0; 4096];
-    let mut write_buffer = buffer::RefWriteBuffer::new(&mut buffer);
-
-    loop {
-        let result = try!(decryptor.decrypt(&mut read_buffer, &mut write_buffer, true));
-        final_result.extend(write_buffer.take_read_buffer().take_remaining().iter().map(|&i| i));
         match result {
             BufferResult::BufferUnderflow => break,
             BufferResult::BufferOverflow => { }
@@ -109,106 +41,24 @@ fn decrypt(encrypted_data: &[u8], key: &[u8], iv: &[u8]) -> Result<Vec<u8>, symm
 }
 
 fn main() {
-    let message = "Cry";
+    let message = "Cry Havoc, and let slip the dogs of war";
+    let password = "testpass";
 
-    let mut key: [u8; 16] = [0x39, 0x28, 0xF8, 0x60,
-                             0xd8, 0x8a, 0x89, 0x1d,
-                             0x3e, 0xb5, 0xc8, 0x7a,
-                             0x71, 0x83, 0x29, 0x08];
-    let mut test: [u8; 16] = [0xF0, 0xDD, 0xC8, 0x54, 0xB7, 0x6B, 0x67, 0xB3, 0x87, 0x38, 0xE6, 0x5, 0x36, 0x82, 0xD9, 0xA2];
-    let mut iv: [u8; 16] = [0xdb, 0xf2, 0x01, 0xd4, 
-                            0x13, 0x0a, 0x01, 0xd4, 
-                            0x53, 0x22, 0x01, 0xd4, 
-                            0x45, 0x5c, 0x01, 0xd5];
-    let mut tmp: [u8; 16] = [0; 16];
+    let mut key: [u8; 16] = [0; 16];
+    let mut salt: [u8; 16] = [0; 16];
+    let mut iv: [u8; 16] = [0; 16];
 
-    // In a real program, the key and iv may be determined
-    // using some other mechanism. If a password is to be used
-    // as a key, an algorithm like PBKDF2, Bcrypt, or Scrypt (all
-    // supported by Rust-Crypto!) would be a good choice to derive
-    // a password. For the purposes of this example, the key and
-    // iv are just random values.
     let mut rng = rand::thread_rng();
-    //rng.fill_bytes(&mut key);
-    //rng.fill_bytes(&mut iv);
-    //rng.fill_bytes(&mut test);
+    rng.fill_bytes(&mut iv);
+    rng.fill_bytes(&mut salt);
 
-    //cryptoimpl::aes::run_tests(&mut iv);
+    let sparams = scrypt::ScryptParams::new(4, 5, 6);
 
-    for i in 0..16 {
-        tmp[i] = iv[i];
-        print!("0x{:X}\t", tmp[i]);
-    }
-    println!("\n");
+    scrypt::scrypt(password.as_bytes(), &salt, &sparams, &mut key);
 
-    cryptoimpl::aes::encrypt_block(&mut iv, &mut test);
+    let reference_enc = encrypt(message.as_bytes(), &key, &iv).ok().unwrap();
+    let encrypted_data = cryptoimpl::aes::cbc_encrypt(message.as_bytes(), &key, &iv);
 
-    cryptoimpl::aes::decrypt_block(&mut iv, &mut test);
-
-    for i in 0..16 {
-        print!("0x{:X}\t", iv[i]);
-    }
-    println!("\n");
-
-
-    let encrypted_data = encrypt(message.as_bytes(), &key, &iv).ok().unwrap();
-    let myencrypted = cryptoimpl::aes::cbc_encrypt(message.as_bytes(), &key, &iv);
-    let decrypted_data = decrypt(&encrypted_data[..], &key, &iv).ok().unwrap();
-    let mydecrypted = cryptoimpl::aes::cbc_decrypt(&myencrypted[..], &key, &iv);
-
-    let epath = Path::new("encrypted.txt");
-
-    let mut file = match File::create(&epath) {
-        Err(why) => panic!("couldn't create file {}: {}", epath.display(), why),
-        Ok(stuff) => stuff,
-    };
-
-    match file.write_all(&encrypted_data) {
-        Err(why) => panic!("couldn't write to {}: {}", epath.display(), why),
-        Ok(_) => println!("successfully wrote to {}", epath.display()),
-    }
-
-    let mpath = Path::new("myencrypted.txt");
-
-    let mut file = match File::create(&mpath) {
-        Err(why) => panic!("couldn't create file {}: {}", mpath.display(), why),
-        Ok(stuff) => stuff,
-    };
-
-    match file.write_all(&myencrypted) {
-        Err(why) => panic!("couldn't write to {}: {}", mpath.display(), why),
-        Ok(_) => println!("successfully wrote to {}", mpath.display()),
-    }
-
-    let dpath = Path::new("decrypted.txt");
-
-    let mut file = match File::create(&dpath) {
-        Err(why) => panic!("couldn't create file {}: {}", dpath.display(), why),
-        Ok(file) => file,
-    };
-
-    match file.write_all(&decrypted_data) {
-        Err(why) => panic!("couldn't write to {}: {}", dpath.display(), why),
-        Ok(_) => println!("successfully wrote to {}", dpath.display()),
-    }
-
-    let lpath = Path::new("mydecrypted.txt");
-
-    let mut file = match File::create(&lpath) {
-        Err(why) => panic!("couldn't create file {}: {}", lpath.display(), why),
-        Ok(file) => file,
-    };
-
-    match file.write_all(&mydecrypted) {
-        Err(why) => panic!("couldn't write to {}: {}", lpath.display(), why),
-        Ok(_) => println!("successfully wrote to {}", lpath.display()),
-    }
-
-    for i in 0..encrypted_data.len() {
-        println!("0x{:X}\t0x{:X}", encrypted_data[i], myencrypted[i]);
-    }
-
-    assert!(encrypted_data.len() == myencrypted.len());
-
-    assert!(message.as_bytes() == &decrypted_data[..]);
+    assert!(&reference_enc[..] == &encrypted_data[..]);
+    println!("Test completed successfully");
 }
