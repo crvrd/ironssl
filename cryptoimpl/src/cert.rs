d51d23bf337b2ba92694f2bf4b2357b896c69a19
David Greer <greerdavidc@gmail.com>
2015-05-15 15:43:27 -0500

    moved things into single library

---
+++ cryptoimpl/src/cert.rs
@@ -0,0 +1,19 @@
+// Client Demo
+
+use std::net::TcpStream;
+use std::io::Write;
+// use std::io::net::tcp::TcpStream;
+
+fn main() {
+	
+	let mut stream = TcpStream::connect("127.0.0.1:4000").unwrap();
+
+	// let mut buf1 = "hello";
+	stream.write(b"hello");
+	
+	// let mut buf = "hi";
+	// stream.read(&buf);
+	// println!("{:?}", buf);
+	
+	drop(stream); // close the connection
+}
